3e94eeeb3cf4edb7a0381946288eec6179f2733a
sleepyhoon <pokjm99@gmail.com>
2024-11-12 16:42:14 +0900

    feat : BJ12764 제출

--- sleepyhoon/heap/BJ12764.java
+++ sleepyhoon/heap/BJ12764.java
@@ -3,16 +3,17 @@ package sleepyhoon.heap;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
-// 한 1시간쓴듯.
 public class BJ12764 {
+
+    static PriorityQueue<Human> humans = new PriorityQueue<>((o1, o2) -> o1.start - o2.start);
+    static int n;
+    static PriorityQueue<Computer> wait = new PriorityQueue<>(((o1, o2) -> o1.index - o2.index));
+    static PriorityQueue<Computer> use = new PriorityQueue<>(((o1, o2) -> o1.end - o2.end));
+    static int[] arr;
+
     static class Human {
         int start;
         int end;
@@ -22,54 +23,67 @@ public class BJ12764 {
             this.end = end;
         }
     }
+
     static class Computer {
-        Human man;
         int index;
-        int count;
+        int end;
 
-        Computer(Human man, int index, int count) {
-            this.man = man;
+        Computer(int index) {
             this.index = index;
-            this.count = count;
-        }
-
-        public Integer getIndex() {
-            return this.index;
+            this.end = -1;
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        // 최대 몇 개의 컴퓨터가 필요한지 구해야 한다.
-        PriorityQueue<Human> humans = new PriorityQueue<>((o1, o2) -> o1.start - o2.start);
-        PriorityQueue<Computer> wait = new PriorityQueue<>((o1,o2)->o1.index - o2.index);
-        Queue<Computer> use = new ArrayDeque<>();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n+1];
         for(int i=0;i<n;i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             humans.offer(new Human(start,end));
-            wait.offer(new Computer(null,i+1,0));
+            Computer newCom = new Computer(i);
+            wait.offer(newCom);
         }
 
+        // 초기화
+        Human poll = humans.poll();
+        Computer computer = wait.poll();
+        computer.end = poll.end;
+        arr[computer.index]++;
+        use.offer(computer);
         while(!humans.isEmpty()) {
-            Human human = humans.poll();
-            int currentTime = human.start;
-            for(int i=0;i<use.size();i++){
-                Computer polled = use.poll();
-                if(polled.man.end < currentTime) {
-                    polled.man = null;
-                    wait.offer(polled);
-                    continue;
+            Human man = humans.poll();
+            int currentTime = man.start;
+
+            while(!use.isEmpty()) {
+                // 시간 정리
+                Computer computer1 = use.poll();
+                if(computer1.end < currentTime) {
+                    computer1.end = -1;
+                    wait.offer(computer1);
                 }
-                use.offer(polled);
+                else {
+                    use.offer(computer1);
+                    break;
+                }
             }
-            Computer computer = wait.poll();
-            computer.man = human;
-            computer.count++;
-            use.offer(computer);
+
+            Computer com = wait.poll();
+            com.end = man.end;
+            arr[com.index]++;
+            use.offer(com);
         }
+        int maxSize = 0;
+        for (int i : arr) {
+            if(i==0) break;
+            maxSize++;
+            sb.append(i).append(" ");
+        }
+        System.out.println(maxSize);
+        System.out.println(sb);
     }
 }
