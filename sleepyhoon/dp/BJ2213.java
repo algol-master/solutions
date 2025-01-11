d06fb62338e341d91c73f05bf0879b6ebf30dc9a
sleepyhoon <pokjm99@naver.com>
2025-01-11 21:29:56 +0900

    feat : 문제 제출

--- sleepyhoon/dp/BJ2213.java
+++ sleepyhoon/dp/BJ2213.java
@@ -1,10 +1,10 @@
+package sleepyhoon.dp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ2213 {
