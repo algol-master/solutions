7a753f82694d2a0437e1b5e88daec4beea500fc8
sleepyhoon <pokjm99@naver.com>
2025-01-25 21:59:06 +0900

    feat : 20250125 제출

--- sleepyhoon/binarySearch/BJ6236.java
+++ sleepyhoon/binarySearch/BJ6236.java
@@ -1,7 +1,8 @@
+package sleepyhoon.binarySearch;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ6236 {
